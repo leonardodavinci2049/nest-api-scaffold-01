42c39c1180d2b67b77b1c9a2ab82b82b599ad2f5
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2024-11-19 18:58:33 -0300

    :fire: chore: Prisma Package

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -8,20 +8,24 @@ import {
 export class CreateUserDto {
   @IsOptional()
   @IsUUID(4, { message: 'ID_UUID must be a valid UUIDv4' })
-  ID_UUID;
+  uuid;
 
   @IsString({ message: 'LOGIN must be a valid string', each: true })
-  LOGIN?: string;
+  name?: string;
 
   @IsStrongPassword(
     {
       minLength: 8,
-      minNumbers: 20,
+      minNumbers: 2,
       minLowercase: 1,
       minUppercase: 1,
       minSymbols: 1,
     },
     { message: 'Password is too weak' },
   )
-  SENHA?: string;
+  password?: string;
+
+  createdAt?: Date;
+
+  updatedAt?: Date;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,31 +1,149 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { v4 as UuidV4 } from 'uuid';
+
+import { PrismaService } from 'src/core/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    createUserDto.ID_UUID = UuidV4();
-    return `{
-      ID_UUID: ${createUserDto.ID_UUID},
-      LOGIN : ${createUserDto.LOGIN},
-      SENHA: ${createUserDto.SENHA}            
-   }`;
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const offset = new Date().getTimezoneOffset() * 60000;
+      createUserDto.createdAt = new Date(Date.now() - offset);
+      createUserDto.updatedAt = new Date(Date.now() - offset);
+
+      createUserDto.uuid = UuidV4();
+
+      const password = await bcrypt.hash(createUserDto.password, 10);
+
+      return await this.prisma.tbl_user.create({
+        data: {
+          uuid: createUserDto.uuid,
+          name: createUserDto.name,
+          password: password,
+          createdAt: createUserDto.createdAt,
+          updatedAt: createUserDto.updatedAt,
+        },
+        select: {
+          id: true,
+          uuid: true,
+          name: true,
+          password: true,
+          // ROLE: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    // USING PRISMA
+
+    try {
+      return this.prisma.tbl_user.findMany({
+        where: {
+          name: {
+            contains: 'o',
+          },
+        },
+        take: 10,
+        orderBy: {
+          id: 'desc',
+        },
+        select: {
+          id: true,
+          uuid: true,
+          name: true,
+          password: true,
+          // ROLE: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      console.log('CLOSED CONNECTION 1');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    //there is the option to use findFirst or findMany but findUnique is more performant
+
+    if (!id) throw new NotFoundException(`User with id not found`);
+    // console.log('id: ' + id);
+    await this.userExists(id);
+
+    //console.log('id3: ' + id);
+    return this.prisma.tbl_user.findUnique({
+      where: {
+        id: id,
+      },
+      select: {
+        id: true,
+        uuid: true,
+        name: true,
+        password: true,
+        // ROLE: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, data: UpdateUserDto) {
+    await this.userExists(id);
+
+    // data.SENHA = await bcrypt.hash(SENHA, 10);
+
+    return this.prisma.tbl_user.update({
+      where: {
+        id: id,
+      },
+      data,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async updatePartial(id: number, data: UpdateUserDto) {
+    await this.userExists(id);
+
+    if (data.name == null || data.name == '') {
+      throw new NotFoundException('Name is required.');
+    }
+
+    return this.prisma.tbl_user.update({
+      data: data,
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async remove(id: number) {
+    await this.userExists(id);
+
+    return this.prisma.tbl_user.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async userExists(id: number) {
+    const user = await this.prisma.tbl_user.count({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User  id: ${id} not found.`);
+    }
   }
 }
